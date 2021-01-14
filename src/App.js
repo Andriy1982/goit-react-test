c885cf6637e6806dbfff59b84561d0f7523da752
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-01-14 17:02:32 +0200

    start test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import TodosView from './views/TodosView';
+import CounterView from './views/CounterView';
+
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <Link to="/counter">Счётчик</Link>
+      </li>
+      <li>
+        <Link to="/todos">Заметки</Link>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route path="/counter">
+        <CounterView />
+      </Route>
+
+      <Route path="/todos">
+        <TodosView />
+      </Route>
+    </Switch>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import 'modern-normalize/modern-normalize.css';
+import './styles/base.scss';
+import store from './redux/store';
+
+console.log(store);
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
